3dca824272dd11d22a60b9ddf1164fea1e3438da
christinekarimi <karimikim3@gmail.com>
2018-04-12 12:18:02 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+
+class Config:
+
+    MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    
+
+
+class ProdConfig(Config):
+     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+     pass
+
+
+class DevConfig(Config):
+     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://karimi:karimi@localhost/watchlist'
+DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
