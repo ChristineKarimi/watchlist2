c28bbaad846905630cead85e9abbc9fba28ae061
christinekarimi <karimikim3@gmail.com>
2018-04-13 12:33:40 +0300

    Installation of flask-uploads,importing of uploadset and definition of files that are being uploaded

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 from flask_login import LoginManager
 
@@ -10,7 +11,7 @@ db = SQLAlchemy()
 
 login_manager = LoginManager()
 
-
+photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
 
     app = Flask(__name__)
