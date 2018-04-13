9dd434b4a0e866be1020f980463af2ca54cf9c9a
christinekarimi <karimikim3@gmail.com>
2018-04-13 12:57:17 +0300

    Imported the mail class from flask and initialized the extension in the init.py file

--- app/__init__.py
+++ app/__init__.py
@@ -3,18 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_uploads import UploadSet,configure_uploads,IMAGES
-
+from flask_mail import Mail
 from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 login_manager = LoginManager()
+mail = Mail()
 
 photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
 
     app = Flask(__name__)
+
+    mail.init_app(app)
 
     # Creating the app configurations.
     app.config.from_object(config_options[config_name])
